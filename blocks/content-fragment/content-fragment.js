3e6b9d3e5aa4263183c68e1b8dd893146e7ccf06
raulugarte <63106469+raulugarte@users.noreply.github.com>
2024-07-03 14:53:06 +0200

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -28,8 +28,10 @@ export default function decorate(block) {
 
   /* RUG */
   const slug2 = slug[3].slice(slug[3].indexOf('>')+1,slug[3].indexOf('<'));
-  const requestRUG = aem + '/graphql/execute.json/aem-demo-assets/adventure-by-slug;slug=' + slug2;
+  /* const requestRUG = aem + '/graphql/execute.json/aem-demo-assets/adventure-by-slug;slug=' + slug2; */
+  const requestRUG = aem + '/graphql/execute.json/bs/article-by-slug;slug=' + slug2'
 
+  
   console.log(slug2);
   console.log(requestRUG);
 
